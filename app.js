d3fccb7ad7b30105f9aa927882b541f474f6df9c
ybybs <81066757+ybybs@users.noreply.github.com>
2025-02-06 19:13:37 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -43,15 +43,20 @@ if (closeButton) {
 const paymentButton = document.getElementById("payment-button");
 if (paymentButton) {
   paymentButton.addEventListener("click", () => {
+    tg.showAlert("Payment ");
+
     // 创建支付发票
-    const invoice = tg.createInvoiceLink(
-        "Title", //title
-        "Some description", //description
-        "{}", //payload
-        "", // For Telegram Stars payment this should be empty
-        "XTR", //currency
-        [{ amount: 1, label: "Diamond" }], //prices
-      );
+    const invoice = {
+      title: "Premium Subscription", // 商品标题
+      description: "Get access to premium features for 1 month.", // 商品描述
+      currency: "XTR", // 货币类型
+      prices: [
+        { label: "1 Month", amount: "5" } // 价格（以最小单位表示，例如 500 = $5.00）
+      ],
+      payload: { // 自定义数据，支付成功后返回
+        userId: tg.initDataUnsafe.user?.id || "unknown"
+      }
+    };
 
     // 打开支付界面
     tg.openInvoice(invoice, function (status) {
